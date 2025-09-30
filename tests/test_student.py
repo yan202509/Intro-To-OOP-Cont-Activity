93bb2e3a065b87ea7f5fcd3a983d11a4c081f1db
YanYi Chen <vickyeah13@gmail.com>
2025-09-30 11:53:31 -0700

    add test

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,3 +1,51 @@
 from school_schedule.student import Student
 
-# Write your tests here!
+
+def test_student_class_iniitialization():
+    # Arrange
+    name = "Bob"
+    grade = "Junior"
+    classes = ["History", "CS"]
+
+
+    # Act
+
+    bob = Student(name, grade, classes)
+
+    # Assert
+    
+    assert bob.name == name
+    assert bob.grade == grade
+    assert bob.classes == classes
+
+
+def test_add_class_allows_non_string_values():
+
+    alice = Student("Alice", "Senior", ["History", "CS"])
+
+    result = alice.add_class(123)
+
+    assert result == ["History", "CS", 123]
+
+
+
+def test_get_student_num_classes():
+    # Arrange
+    name = "Kate"
+    grade = "Junior"
+    classes = ["History", "CS"]
+
+    kate = Student(name, grade, classes)
+
+    # Act
+
+    result = kate.get_num_classes()
+
+    # Assert
+    
+    assert result == 2
+
+
+
+
+
